071788531907a033bf7084308bb0dc068fd39c90
Saksham Jain <sakshamj010@gmail.com>
2021-05-14 21:32:31 +0530

    Add files via upload

---
+++ speech to text.py
@@ -0,0 +1,25 @@
+#import library
+import speech_recognition as sr
+filename="I-dont-know.wav"
+# Initialize recognizer class (for recognizing the speech)
+r = sr.Recognizer()
+
+# Reading Audio file as source
+# listening the audio file and store in audio_text variable
+
+with sr.AudioFile('I-dont-know.wav') as source:
+
+    audio_text = r.listen(source)
+
+    # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
+    try:
+
+        # using google speech recognition
+        text = r.recognize_google(audio_text)
+        print('Converting audio transcripts into text ...')
+        print(text)
+
+    except:
+        print('Sorry.. run again...')
+
+
